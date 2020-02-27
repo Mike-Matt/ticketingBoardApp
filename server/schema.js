8a46287c27c64b68e666ebdaec389f4a21a5ad53
Michael Thompson <michael.220488@gmail.com>
2020-02-27 15:15:37 -0800

    updated

--- server/schema.js
+++ server/schema.js
@@ -6,11 +6,38 @@ const UserType = new GraphQLObjectType({
   fields: ()=> ({
     user_id: { type: GraphQLInt },
     user_name: { type: GraphQLString },
-    email: { type: GraphQLString },
-    password: { type: GraphQLString },
+    user_email: { type: GraphQLString },
+    user_password: { type: GraphQLString },
     board: { type: BoardType }
   })
 })
 
 //Board
-const BoardType
+const BoardType = new GraphQLObjectType({
+  name: 'Board',
+  fields: ()=> ({
+    board_id: { type: GraphQLInt },
+    board_name: { type: GraphQLString },
+    column: { type: ColumnType }
+  })
+})
+
+//Column
+const ColumnType = new GraphQLObjectType({
+  name: 'Column',
+  fields: ()=> ({
+    column_id: { type: GraphQLInt},
+    column_name: { type: GraphQLString },
+    ticket: { type: TicketType }
+  })
+})
+
+//Ticket
+const TicketType = new GraphQLObjectType({
+  name: 'Ticket',
+  fields: ()=> ({
+    ticket_id: { type: GraphQLInt},
+    ticket_name: { type: GraphQLString },
+    ticket_text: { type: GraphQLString }
+  })
+})
