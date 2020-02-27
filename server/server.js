124dfc02880ece295e964358215453c4536d4976
Michael Thompson <michael.220488@gmail.com>
2020-02-27 14:46:01 -0800

    working on GraphQL schema

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const app = express()
 const PORT = 4000
+const { TicketingAppSchema } = require('./schema.js')
 
 app.use('/graphql', graphqlHTTP({
   schema: TicketingAppSchema,
