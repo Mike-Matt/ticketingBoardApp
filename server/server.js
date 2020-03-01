6270f020ee7b982a0b3b5c9c2cdcfeebd307d879
Matthew Yee <matthewyee@Matthews-MBP-61.lan>
2020-02-29 21:51:25 -0600

    added mongoose connection to the server - working.

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const resolvers = require("./resolvers");
 const { ApolloServer } = require("apollo-server-express");
 const typeDefs = require("./schema.js");
+const mongoose = require("mongoose"); 
 
 const app = express();
 const PORT = 4000;
 
-
 const server = new ApolloServer({ typeDefs });
+
+mongoose.connect('mongodb+srv://mylife-ajkjc.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true });
+
 server.applyMiddleware({ app });
 app.listen({ port: PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
