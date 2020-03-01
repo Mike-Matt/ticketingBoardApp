4f46b0b38507352839f89c2d192fa16fb20970d6
Matthew Yee <matthewyee@Matthews-MBP-61.lan>
2020-02-29 21:34:25 -0600

    added mongoose package and fixed formatting across all files.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const { ApolloServer } = require('apollo-server-express')
-const app = express()
-const PORT = 4000
-const typeDefs  = require('./schema.js')
+const express = require("express");
+const resolvers = require("./resolvers");
+const { ApolloServer } = require("apollo-server-express");
+const app = express();
+const PORT = 4000;
+const typeDefs = require("./schema.js");
 
 const server = new ApolloServer({ typeDefs });
 server.applyMiddleware({ app });
 app.listen({ port: PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-)
+);
