0acbadc33f36f585047bbd4fc5075e273696480e
Michael Thompson <michael.220488@gmail.com>
2020-02-29 21:06:12 -0800

    connected to mongodb_atlas

--- server/server.js
+++ server/server.js
@@ -5,9 +5,17 @@ const PORT = 4000
 const typeDefs  = require('./schema.js')
 const resolvers = require('./resolvers.js')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const server = new ApolloServer({ typeDefs, resolvers })
-mongoose.connect("mongodb+srv://mylife-ajkjc.mongodb.net/test", {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb+srv://michael_thompson:${process.env.MongoDB_password}@mylife-ajkjc.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true }, (err)=>{
+  if(err) {
+    console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+  }
+  else{
+    console.log('Connected...');
+  }
+})
 server.applyMiddleware({ app })
 app.listen({ port: PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
